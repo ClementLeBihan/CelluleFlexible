d03b84b5039e3376fadaa15b19defdc6b7ea0528
etudiant <clelebi@hotmail.fr>
2016-02-09 11:34:54 +0100

    Circuit Infini : 4 Capteurs + Aiguillage from ROS NODE

--- catkin_ws/src/simulation/src/simulation.cpp
+++ catkin_ws/src/simulation/src/simulation.cpp
@@ -40,6 +40,9 @@ int capteur1, capteur2, capteur3, capteur4;
 void CapteurCallback(const std_msgs::Int32::ConstPtr& msg)
 {
 	capteur1 = (msg->data & 1) == 1;
+	capteur2 = (msg->data & 2) == 2;
+	capteur3 = (msg->data & 4) == 4;
+	capteur4 = (msg->data & 8) == 8;
 }
 
 
@@ -54,6 +57,22 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
 }
+
+
+string float2string(float f)
+{
+  ostringstream os;
+  os << f;
+  return os.str();
+}
+
+string int2string(int f)
+{
+  ostringstream os;
+  os << f;
+  return os.str();
+}
+
 
 
 int main(int argc, char **argv)
@@ -89,24 +108,57 @@ int main(int argc, char **argv)
 
 			///////// VARIABLES ////////
 				std_msgs::Float64 velo30, velo0;
-				velo30.data = 30;
-				velo0.data = 0;
+				int handle_A1, handle_sol;
+				int consigne = 0;
+				ros::spinOnce();
+
+			////////// VREP HANDLE  ///////
+				ros::ServiceClient client_getObjectHandle=nh.serviceClient<vrep_common::simRosGetObjectHandle>("/vrep/simRosGetObjectHandle");
+				vrep_common::simRosGetObjectHandle srv_getObjectHandle;
+				
+				// Aiguillage 1
+				srv_getObjectHandle.request.objectName="Aiguillage_1";
+				if ( client_getObjectHandle.call(srv_getObjectHandle))
+				    handle_A1 = srv_getObjectHandle.response.handle;
+
+				// Sol
+				srv_getObjectHandle.request.objectName="ResizableFloor_5_25";
+				if ( client_getObjectHandle.call(srv_getObjectHandle))
+				    handle_sol = srv_getObjectHandle.response.handle;
+
+			//////// VREP  SetObjectQuaternion /////
+				ros::ServiceClient client_SetObjectQuaternion=nh.serviceClient<vrep_common::simRosSetObjectQuaternion>("/vrep/simRosSetObjectQuaternion");
+				vrep_common::simRosSetObjectQuaternion srv_SetObjectQuaternion;
+
+				srv_SetObjectQuaternion.request.handle = handle_A1;				
+				srv_SetObjectQuaternion.request.relativeToObjectHandle = handle_sol;
 
 			while (ros::ok())
 			{
 				ros::spinOnce();
+				
+				//On donne le quaternion voulu en fonction de la position (en fonction des états des capteurs)
 	
-				if (capteur1) {
-					MotorVelocity.publish(velo0);
-					sleep(2);
-					do
-					{
-						MotorVelocity.publish(velo30);
-						ros::spinOnce();
-					} while(capteur1);
+				if ((capteur2||capteur3) && consigne == 0) {
+					srv_SetObjectQuaternion.request.quaternion.x=0.26318;
+					srv_SetObjectQuaternion.request.quaternion.y=0.656306;
+					srv_SetObjectQuaternion.request.quaternion.z=0.26293;
+					srv_SetObjectQuaternion.request.quaternion.w=-0.656408;
+					consigne = 1; 
 				}
+				else if ((capteur1||capteur4) && consigne == 0) {
+					srv_SetObjectQuaternion.request.quaternion.x=-0.27806;
+					srv_SetObjectQuaternion.request.quaternion.y=0.65000;
+					srv_SetObjectQuaternion.request.quaternion.z=-0.27816;
+					srv_SetObjectQuaternion.request.quaternion.w=-0.65024;
+					consigne = 1;
+				}
+				else consigne = 0;
+				
+				if(consigne == 1) 
+					client_SetObjectQuaternion.call(srv_SetObjectQuaternion);
 
-				else MotorVelocity.publish(velo30);
+
 			}	
 			cv::destroyWindow("view");
 			return 0;
